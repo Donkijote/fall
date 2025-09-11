29177a2b022a249943968b29eaecac92405597a0
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-09-11 15:31:01 +0200

    feat: distributing 2 players across the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { clsx } from "clsx";
 import { useEffect } from "react";
 
 import { useGameStore } from "@/application/store/gameStore";
 import { Card } from "@/components/card/Card";
-import { CardList } from "@/components/card/CardList";
 
 import "./App.css";
 
@@ -57,44 +57,84 @@ function App() {
           </button>
         </div>
       </div>
-      <div className={"pt-8 gap-4 flex justify-center"}>
-        {state.table.map((card) => (
-          <Card
-            key={card.suit + card.rank}
-            rank={card.rank}
-            suit={card.suit}
-            disabled={true}
-          />
-        ))}
-      </div>
-      <div className={"pt-12 gap-20 flex items-center justify-center"}>
-        {state.players.map((player) => (
-          <div
-            className={"gap-8 flex flex-col items-center justify-center"}
-            key={player.id}
-          >
-            <div className={"flex w-full flex-row"}>
-              <p className={"w-full text-center"}>{player.id}</p>
-              {player.id === state.dealer && <p className={"self-end"}>D!</p>}
-            </div>
-            <div className={"gap-4 flex flex-row items-center justify-center"}>
-              {player.hand.map((card, index) => (
-                <Card
-                  key={card.suit + card.rank}
-                  rank={card.rank}
-                  suit={card.suit}
-                  onClick={() => service.playCard(player.id, index)}
-                  disabled={player.id !== state.currentPlayer}
-                  className={
-                    player.id === state.currentPlayer ? "cursor-pointer" : ""
-                  }
-                />
-              ))}
-            </div>
+      <div
+        className={
+          "border-gray-700 relative h-[calc(100vh-120px)] w-full border"
+        }
+      >
+        <div
+          className={"flex h-full w-full flex-wrap items-center justify-center"}
+        >
+          <div className={"gap-8 flex w-1/6 flex-wrap"}>
+            {state.table.map((card) => (
+              <Card
+                key={card.suit + card.rank}
+                rank={card.rank}
+                suit={card.suit}
+                disabled={true}
+              />
+            ))}
           </div>
-        ))}
+        </div>
+        <>
+          {state.players.map((player, index) => (
+            <div
+              className={clsx("absolute w-full", {
+                "top-10": index % 2 === 0,
+                "bottom-10": index % 2 !== 0,
+              })}
+            >
+              <div
+                className={
+                  "gap-8 relative flex flex-col items-center justify-center"
+                }
+                key={player.id}
+              >
+                <div
+                  className={clsx("absolute", {
+                    "-bottom-10": index % 2 === 0,
+                    "-top-10": index % 2 !== 0,
+                  })}
+                >
+                  <div className={"flex w-full flex-row"}>
+                    <p className={"w-full text-center"}>{player.id}</p>
+                    {player.id === state.dealer && (
+                      <p className={"self-end"}>D!</p>
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={"gap-4 flex flex-row items-center justify-center"}
+                >
+                  {player.hand.map((card, cIndex) => (
+                    <Card
+                      key={card.suit + card.rank}
+                      rank={card.rank}
+                      suit={card.suit}
+                      onClick={() => service.playCard(player.id, cIndex)}
+                      disabled={player.id !== state.currentPlayer}
+                      faceDown={player.id !== state.currentPlayer}
+                      className={clsx(
+                        player.id === state.currentPlayer
+                          ? "cursor-pointer"
+                          : "",
+                        {
+                          "mt-4 -rotate-14": index % 2 !== 0 && cIndex === 0,
+                          "mb-4": index % 2 !== 0 && cIndex === 1,
+                          "mt-4 rotate-14": index % 2 !== 0 && cIndex === 2,
+                          "mb-4 rotate-14": index % 2 === 0 && cIndex === 0,
+                          "mt-4": index % 2 === 0 && cIndex === 1,
+                          "mb-4 -rotate-14": index % 2 === 0 && cIndex === 2,
+                        },
+                      )}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
       </div>
-      <CardList />
     </div>
   );
 }
