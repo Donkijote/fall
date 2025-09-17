1176e3d0501e6334700833236e8b979fddcb901e
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-09-17 19:08:45 +0200

    feat: positioning cards randomly on the table

--- src/infrastructure/ui/components/table/Table.tsx
+++ src/infrastructure/ui/components/table/Table.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 
 import { useGameStoreState } from "@/application/hooks/useGameStoreState";
+import type { Card as ICard } from "@/domain/entities/Card";
 import { Card } from "@/infrastructure/ui/components/card/Card";
 
 import { ItalianTable } from "./ItalianTable";
@@ -17,20 +18,36 @@ export const Table = ({ children }: { children: ReactNode }) => {
     <div className={"relative h-[calc(100vh-120px)] w-full"}>
       {TableStyles[1]}
       <div
-        className={"flex h-full w-full flex-wrap items-center justify-center"}
+        className={
+          "absolute top-1/2 left-1/2 h-[40%] w-[40%] -translate-x-1/2 -translate-y-1/2 transform"
+        }
       >
-        <div className={"gap-8 flex w-1/6 flex-wrap"}>
-          {table.map((card) => (
-            <Card
-              key={card.suit + card.rank}
-              rank={card.rank}
-              suit={card.suit}
-              disabled={true}
-            />
-          ))}
-        </div>
-        {children}
+        {table.map((card) => (
+          <TableCard key={card.suit + card.rank} card={card} />
+        ))}
       </div>
+      {children}
     </div>
+  );
+};
+
+type TableCardProps = {
+  card: ICard;
+};
+
+const TableCard = ({ card }: TableCardProps) => {
+  const randomPositionLeft = `${Math.floor(Math.random() * 65)}%`;
+  const randomPositionTop = `${Math.floor(Math.random() * 60)}%`;
+  return (
+    <Card
+      rank={card.rank}
+      suit={card.suit}
+      disabled={true}
+      style={{
+        left: randomPositionLeft,
+        top: randomPositionTop,
+        position: "absolute",
+      }}
+    />
   );
 };
