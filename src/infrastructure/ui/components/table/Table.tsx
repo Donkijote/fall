15b7734b071a75551546a6ef27c19457eefbd002
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-09-17 17:13:14 +0200

    feat: adjust table to render table cards

--- src/infrastructure/ui/components/table/Table.tsx
+++ src/infrastructure/ui/components/table/Table.tsx
@@ -1,5 +1,8 @@
 import type { ReactNode } from "react";
 
+import { useGameStoreState } from "@/application/hooks/useGameStoreState";
+import { Card } from "@/infrastructure/ui/components/card/Card";
+
 import { ItalianTable } from "./ItalianTable";
 import { PokerTable } from "./PokerTable";
 import { WoodTable } from "./WoodTable";
@@ -7,7 +10,8 @@ import { WoodTable } from "./WoodTable";
 const TableStyles = [<ItalianTable />, <PokerTable />, <WoodTable />];
 
 export const Table = ({ children }: { children: ReactNode }) => {
-  const randomIndex = Math.floor(Math.random() * TableStyles.length);
+  // const randomIndex = Math.floor(Math.random() * TableStyles.length);
+  const { table } = useGameStoreState();
 
   return (
     <div className={"relative h-[calc(100vh-120px)] w-full"}>
@@ -15,6 +19,16 @@ export const Table = ({ children }: { children: ReactNode }) => {
       <div
         className={"flex h-full w-full flex-wrap items-center justify-center"}
       >
+        <div className={"gap-8 flex w-1/6 flex-wrap"}>
+          {table.map((card) => (
+            <Card
+              key={card.suit + card.rank}
+              rank={card.rank}
+              suit={card.suit}
+              disabled={true}
+            />
+          ))}
+        </div>
         {children}
       </div>
     </div>
