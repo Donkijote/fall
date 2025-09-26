8ae48409010d5c9763da99e0ba44832390777abd
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-09-26 13:51:22 +0200

    feat: add new game screen

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
+import { GameScreen } from "@/infrastructure/ui/screens/Game";
 import { HomeScreen } from "@/infrastructure/ui/screens/Home";
+
+import { GAME_PATH, HOME_PATH } from "./Routes";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: HOME_PATH,
     element: <HomeScreen />,
+  },
+  {
+    path: GAME_PATH,
+    element: <GameScreen />,
   },
 ]);
 
