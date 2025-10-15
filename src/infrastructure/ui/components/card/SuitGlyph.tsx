9057995c91999964d24ff2bcf124f6e4b4cbfbde
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-10-15 13:14:39 +0200

    refactor: all clubs cards designs

--- src/infrastructure/ui/components/card/SuitGlyph.tsx
+++ src/infrastructure/ui/components/card/SuitGlyph.tsx
@@ -1,6 +1,15 @@
-import { clsx } from "clsx";
 import type { FC, PropsWithChildren } from "react";
 
+import AceOfClubs from "@/assets/cards/clubs/AceOfClubs.svg";
+import ElevenOfClubs from "@/assets/cards/clubs/ElevenOfClubs.svg";
+import FiveOfClubs from "@/assets/cards/clubs/FiveOfClubs.svg";
+import FourOfClubs from "@/assets/cards/clubs/FourOfClubs.svg";
+import SevenOfClubs from "@/assets/cards/clubs/SevenOfClubs.svg";
+import SixOfClubs from "@/assets/cards/clubs/SixOfClubs.svg";
+import TenOfClubs from "@/assets/cards/clubs/TenOfClubs.svg";
+import ThreeOfClubs from "@/assets/cards/clubs/ThreeOfClubs.svg";
+import TwelveOfClubs from "@/assets/cards/clubs/TwelveOfClubs.svg";
+import TwoOfClubs from "@/assets/cards/clubs/TwoOfClubs.svg";
 import AceOfCoins from "@/assets/cards/coins/AceOfCoins.svg";
 import ElevenOfCoins from "@/assets/cards/coins/ElevenOfCoins.svg";
 import FiveOfCoins from "@/assets/cards/coins/FiveOfCoins.svg";
@@ -32,17 +41,6 @@ import ThreeOfSwords from "@/assets/cards/swords/ThreeOfSwords.svg";
 import TwelveOfSwords from "@/assets/cards/swords/TwelveOfSwords.svg";
 import TwoOfSwords from "@/assets/cards/swords/TwoOfSwords.svg";
 import { type Suit, SUIT_COLOR } from "@/domain/entities/Card";
-import {
-  AceOfClubsGlyph,
-  ElevenOfClubs,
-  GreenClubGlyph,
-  RedClubGlyph,
-  TenOfClubs,
-  ThreeOfClubsGlyph,
-  TwelveOfClubs,
-  TwoOfClubsGlyph,
-  YellowClubGlyph,
-} from "@/infrastructure/ui/components/card/ClubGlyph";
 
 type SuitGlyphProps = {
   suit: Suit;
@@ -56,330 +54,61 @@ const LAYOUT: Record<
 > = {
   1: {
     golds: () => <img src={AceOfCoins} alt={"AceOfCoins"} />,
-    clubs: ({ size }) => (
-      <AceOfClubsGlyph
-        className={clsx("absolute", {
-          "-top-1 h-22 -rotate-5": size === "sm",
-          "-top-2 -left-4 h-31 -rotate-6": size === "md",
-          "-top-4 -left-5 h-42 -rotate-8": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={AceOfClubs} alt={"AceOfClubs"} />,
     blades: () => <img src={AceOfSwords} alt={"AceOfSwords"} />,
     cups: () => <img src={AceOfCups} alt={"AceOfCups"} />,
   },
   2: {
     golds: () => <img src={TwoOfCoins} alt={"TwoOfCoins"} />,
-    clubs: ({ color, size }) => (
-      <TwoOfClubsGlyph
-        fill={color}
-        className={clsx("absolute", {
-          "-top-1 -left-3 h-21": size === "sm",
-          "-top-2 -left-4 h-30": size === "md",
-          "-top-4 -left-5 h-40": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={TwoOfClubs} alt={"TwoOfClubs"} />,
     blades: () => <img src={TwoOfSwords} alt={"TwoOfSwords"} />,
     cups: () => <img src={TwoOfCups} alt={"TwoOfCups"} />,
   },
   3: {
     golds: () => <img src={ThreeOfCoins} alt={"ThreeOfCoins"} />,
-    clubs: ({ color, size }) => (
-      <ThreeOfClubsGlyph
-        fill={color}
-        className={clsx("absolute", {
-          "-top-1.5 -left-2.5 h-24": size === "sm",
-          "-top-3 -left-1.5 h-32": size === "md",
-          "-top-4 -left-2 h-42": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={ThreeOfClubs} alt={"ThreeOfClubs"} />,
     blades: () => <img src={ThreeOfSwords} alt={"ThreeOfSwords"} />,
     cups: () => <img src={ThreeOfCups} alt={"ThreeOfCups"} />,
   },
   4: {
     golds: () => <img src={FourOfCoins} alt={"FourOfCoins"} />,
-    clubs: ({ size }) => (
-      <>
-        <GreenClubGlyph
-          className={clsx("absolute -rotate-6", {
-            "-top-0.5 -right-3 h-20": size === "sm",
-            "-top-2 -right-3 h-28": size === "md",
-            "-top-2 -right-2 h-40": size === "lg",
-          })}
-          translate={clsx({
-            "translate(40,0)": size !== "lg",
-            "translate(50,0)": size === "lg",
-          })}
-          scale={"scale(-0.170000,0.170000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-6", {
-            "-top-0.5 -right-1.5 h-20": size === "sm",
-            "-top-2 -right-1 h-28": size === "md",
-            "-top-2 -right-3 h-40": size === "lg",
-          })}
-          translate={"translate(80,0)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-6", {
-            "-bottom-1 -right-0.5 h-20": size === "sm",
-            "-bottom-2 -right-1 h-28": size === "md",
-            "-bottom-3 -right-1 h-40": size === "lg",
-          })}
-          translate={clsx({
-            "translate(30,100)": size !== "md",
-            "translate(25,100)": size === "md",
-          })}
-          scale={"scale(0.180000,-0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute -rotate-6", {
-            "-bottom-1 -right-0.5 h-20": size === "sm",
-            "-bottom-2 -right-3.5 h-28": size === "md",
-            "-bottom-3 -right-5 h-40": size === "lg",
-          })}
-          translate={clsx({
-            "translate(60,100)": size === "sm",
-            "translate(50,100)": size === "md",
-            "translate(51,100)": size === "lg",
-          })}
-          scale={"scale(0.170000,-0.170000)"}
-        />
-      </>
-    ),
+    clubs: () => <img src={FourOfClubs} alt={"FourOfClubs"} />,
     blades: () => <img src={FourOfSwords} alt={"FourOfSwords"} />,
     cups: () => <img src={FourOfCups} alt={"FourOfCups"} />,
   },
   5: {
     golds: () => <img src={FiveOfCoins} alt={"FiveOfCoins"} />,
-    clubs: ({ size }) => (
-      <>
-        <GreenClubGlyph
-          className={clsx("absolute rotate-60", {
-            "top-0.5 right-2 h-20": size === "sm",
-            "top-0.5 right-3 h-28": size === "md",
-            "top-0.5 right-4 h-40": size === "lg",
-          })}
-          translate={"translate(0,50)"}
-          scale={"scale(0.170000,-0.170000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute -rotate-60", {
-            "-top-2 -right-2 h-20": size === "sm",
-            "-top-3 -right-3 h-28": size === "md",
-            "-top-4.5 -right-4 h-40": size === "lg",
-          })}
-          translate={"translate(70,70)"}
-          scale={"scale(0.180000,-0.180000)"}
-        />
-        <YellowClubGlyph
-          className={clsx("absolute rotate-90", {
-            "-top-2 left-2 h-20": size === "sm",
-            "-top-4 left-4 h-28": size === "md",
-            "-top-7.5 left-3 h-40": size === "lg",
-          })}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-120", {
-            "-bottom-2 -right-1.5 h-20": size === "sm",
-            "-bottom-3 -right-1.5 h-28": size === "md",
-            "-bottom-4.5 -right-2.5 h-40": size === "lg",
-          })}
-          translate={"translate(70,70)"}
-          scale={"scale(0.180000,-0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute rotate-60", {
-            "-bottom-4.5 right-3 h-20": size === "sm",
-            "-bottom-7 right-4.5 h-28": size === "md",
-            "-bottom-10 right-7 h-40": size === "lg",
-          })}
-          translate={"translate(100,0)"}
-          scale={"scale(-0.170000,0.170000)"}
-        />
-      </>
-    ),
+    clubs: () => <img src={FiveOfClubs} alt={"FiveOfClubs"} />,
     blades: () => <img src={FiveOfSwords} alt={"FiveOfSwords"} />,
     cups: () => <img src={FiveOfCups} alt={"FiveOfCups"} />,
   },
   6: {
     golds: () => <img src={SixOfCoins} alt={"SixOfCoins"} />,
-    clubs: ({ size }) => (
-      <>
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "top-0 -left-3.5 h-20": size === "sm",
-            "-top-1.5 right-0.5 h-28": size === "md",
-            "-top-1.5 -right-0.5 h-40": size === "lg",
-          })}
-          translate={"translate(30,10)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-top-0.5 right-0.5 h-20": size === "sm",
-            "-top-2.5 right-1 h-28": size === "md",
-            "-top-2.5 right-1 h-40": size === "lg",
-          })}
-          translate={"translate(40,50)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "top-0 left-7 h-20": size === "sm",
-            "-top-1.5 left-11 h-28": size === "md",
-            "-top-1.5 left-13.5 h-40": size === "lg",
-          })}
-          translate={"translate(30,10)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-bottom-4.5 right-0 h-20": size === "sm",
-            "-bottom-7 right-1 h-28": size === "md",
-            "-bottom-10 right-0 h-40": size === "lg",
-          })}
-          translate={"translate(85,25)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "-bottom-3 left-1.5 h-20": size === "sm",
-            "-bottom-5 left-3.5 h-28": size === "md",
-            "-bottom-7 left-3.5 h-40": size === "lg",
-          })}
-          translate={"translate(55,55)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-bottom-4.5 left-6.5 h-20": size === "sm",
-            "-bottom-7 left-11 h-28": size === "md",
-            "-bottom-10 left-13.5 h-40": size === "lg",
-          })}
-          translate={"translate(85,25)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-      </>
-    ),
+    clubs: () => <img src={SixOfClubs} alt={"SixOfClubs"} />,
     blades: () => <img src={SixOfSwords} alt={"SixOfSwords"} />,
     cups: () => <img src={SixOfCups} alt={"SixOfCups"} />,
   },
   7: {
     golds: () => <img src={SevenOfCoins} alt={"SevenOfCoins"} />,
-    clubs: ({ size }) => (
-      <>
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "-top-0.5 -left-3.5 h-19": size === "sm",
-            "-top-1.5 right-1 h-28": size === "md",
-            "-top-2.5 right-2 h-37": size === "lg",
-          })}
-          translate={"translate(30,10)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-top-0.5 right-0.5 h-19": size === "sm",
-            "-top-2.5 right-0.5 h-28": size === "md",
-            "-top-3 right-2 h-37": size === "lg",
-          })}
-          translate={"translate(40,50)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "-top-0.5 left-7.5 h-19": size === "sm",
-            "-top-1.5 left-12 h-28": size === "md",
-            "-top-2.5 left-16 h-37": size === "lg",
-          })}
-          translate={"translate(30,10)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <YellowClubGlyph
-          className={clsx("absolute -rotate-90", {
-            "top-1.5 -left-6 h-20": size === "sm",
-            "top-1 -left-7 h-28": size === "md",
-            "top-1 -left-10 h-37": size === "lg",
-          })}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-bottom-4.5 right-0.5 h-19": size === "sm",
-            "-bottom-7 right-1 h-28": size === "md",
-            "-bottom-10 right-2 h-37": size === "lg",
-          })}
-          translate={"translate(85,25)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <GreenClubGlyph
-          className={clsx("absolute rotate-30", {
-            "-bottom-3 left-2.5 h-19": size === "sm",
-            "-bottom-5 left-4 h-28": size === "md",
-            "-bottom-7.5 left-5 h-37": size === "lg",
-          })}
-          translate={"translate(55,55)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-        <RedClubGlyph
-          className={clsx("absolute rotate-155", {
-            "-bottom-4.5 left-7.5 h-19": size === "sm",
-            "-bottom-7 left-12 h-28": size === "md",
-            "-bottom-10 left-16 h-37": size === "lg",
-          })}
-          translate={"translate(85,25)"}
-          scale={"scale(-0.180000,0.180000)"}
-        />
-      </>
-    ),
+    clubs: () => <img src={SevenOfClubs} alt={"SevenOfClubs"} />,
     blades: () => <img src={SevenOfSwords} alt={"SevenOfSwords"} />,
     cups: () => <img src={SevenOfCups} alt={"SevenOfCups"} />,
   },
   10: {
     golds: () => <img src={TenOfCoins} alt={"TenOfCoins"} />,
-    clubs: ({ color, size }) => (
-      <TenOfClubs
-        fill={color}
-        className={clsx("absolute", {
-          "-bottom-3 left-3.5 h-24.5": size === "sm",
-          "-bottom-5 left-5 h-34.5": size === "md",
-          "-bottom-7 left-7 h-44.5": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={TenOfClubs} alt={"TenOfClubs"} />,
     blades: () => <img src={TenOfSwords} alt={"TenOfSwords"} />,
     cups: () => <img src={TenOfCups} alt={"TenOfCups"} />,
   },
   11: {
     golds: () => <img src={ElevenOfCoins} alt={"ElevenOfCoins"} />,
-    clubs: ({ color, size }) => (
-      <ElevenOfClubs
-        fill={color}
-        className={clsx("absolute", {
-          "-bottom-2 left-0 h-23": size === "sm",
-          "-bottom-3 left-0 h-33": size === "md",
-          "-bottom-5 left-0 h-43": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={ElevenOfClubs} alt={"ElevenOfClubs"} />,
     blades: () => <img src={ElevenOfSwords} alt={"ElevenOfSwords"} />,
     cups: () => <img src={ElevenOfCups} alt={"ElevenOfCups"} />,
   },
   12: {
     golds: () => <img src={TwelveOfCoins} alt={"TwelveOfCoins"} />,
-    clubs: ({ color, size }) => (
-      <TwelveOfClubs
-        fill={color}
-        className={clsx("absolute", {
-          "-bottom-2 left-2 h-22": size === "sm",
-          "-bottom-3.5 left-3 h-32": size === "md",
-          "-bottom-5 left-4 h-42": size === "lg",
-        })}
-      />
-    ),
+    clubs: () => <img src={TwelveOfClubs} alt={"TwelveOfClubs"} />,
     blades: () => <img src={TwelveOfSwords} alt={"TwelveOfSwords"} />,
     cups: () => <img src={TwelveOfCups} alt={"TwelveOfCups"} />,
   },
