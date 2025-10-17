fac80bd46757f6cc0ca018bd01700d98fa4acfba
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-10-17 21:45:33 +0200

    refactor: move game over modal to modules folder structure

--- src/infrastructure/ui/screens/Game.tsx
+++ src/infrastructure/ui/screens/Game.tsx
@@ -2,19 +2,15 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 import { useGameStore } from "@/application/store/gameStore";
-import { GameOverModal } from "@/infrastructure/ui/components/modal/GameOverModal";
 import { Players } from "@/infrastructure/ui/components/player/Players";
+import { GameOverModal } from "@/modules/matchEnd/ui/GameOverModal";
 import { Table } from "@/modules/table/ui/Table";
-import { HOME_PATH } from "@/routes/Routes";
 
 export const GameScreen = () => {
   const { state, service } = useGameStore();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (state.phase === "init") {
-      navigate(HOME_PATH);
-    }
     if (state.phase === "deal") {
       service.startGame();
     }
@@ -31,16 +27,7 @@ export const GameScreen = () => {
       <Table>
         <Players />
       </Table>
-      <GameOverModal
-        isOpen={false}
-        onNewGame={() => null}
-        onReplay={() => null}
-        onClose={() => null}
-        result={"win"}
-        stats={{
-          teamScore: [24, 13],
-        }}
-      />
+      <GameOverModal />
     </div>
   );
 };
