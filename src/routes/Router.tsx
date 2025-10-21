ae9f49a190a907137383c63dbd4126af7590a99e
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-10-21 21:58:23 +0200

    fix: routes basename due to github pages requirement

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -7,19 +7,24 @@ import { HomeScreen } from "@/infrastructure/ui/screens/Home";
 
 import { CARDS_PATH, GAME_PATH, HOME_PATH } from "./Routes";
 
-const router = createBrowserRouter([
-  {
-    path: HOME_PATH,
-    element: <HomeScreen />,
-  },
-  {
-    path: GAME_PATH,
-    element: <GameScreen />,
-  },
-  {
-    path: CARDS_PATH,
-    element: <CardList />,
-  },
-]);
+const basename = (import.meta.env.BASE_URL ?? "/").replace(/\/+$/, "");
+
+const router = createBrowserRouter(
+  [
+    {
+      path: HOME_PATH,
+      element: <HomeScreen />,
+    },
+    {
+      path: GAME_PATH,
+      element: <GameScreen />,
+    },
+    {
+      path: CARDS_PATH,
+      element: <CardList />,
+    },
+  ],
+  { basename },
+);
 
 export const AppRouter = () => <RouterProvider router={router} />;
