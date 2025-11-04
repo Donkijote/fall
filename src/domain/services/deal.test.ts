2e06816fee1ebb9829c76cab55224f40930ee214
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-11-04 10:38:45 +0100

    test: remove unnecessary tests

--- src/domain/services/deal.test.ts
+++ src/domain/services/deal.test.ts
@@ -3,7 +3,7 @@ import { expect } from "vitest";
 import { mockedStateWithPlayers } from "@/application/store/gameStore";
 import { createDeck } from "@/domain/rules/deck";
 
-import { applyTablePatternBonus, chooseDealer, dealRound } from "./deal";
+import { applyTablePatternBonus } from "./deal";
 
 const stateWithDealer = {
   ...mockedStateWithPlayers,
@@ -11,46 +11,6 @@ const stateWithDealer = {
 };
 
 describe("Deal", () => {
-  it("should choose dealer", () => {
-    const dealer = chooseDealer(stateWithDealer);
-    expect(dealer.dealer).toBeDefined();
-    expect(dealer.phase).toBe("dealerChoice");
-  });
-  it("should deal table when isDealerFirstDeal is true and players first", () => {
-    const state = chooseDealer(stateWithDealer);
-    const result = dealRound(state, { isDealerFirstDeal: true });
-    expect(result.table.length).toBe(4);
-    for (const p of result.players) {
-      expect(p.hand.length).toBe(3);
-    }
-  });
-  it("should deal table when isDealerFirstDeal is true and table first", () => {
-    const state = chooseDealer(stateWithDealer);
-    const deckWithDuplicatedCard = [...state.deck];
-    deckWithDuplicatedCard.unshift(deckWithDuplicatedCard[0]);
-    const result = dealRound(
-      { ...state, deck: deckWithDuplicatedCard },
-      {
-        isDealerFirstDeal: true,
-        tablePattern: "dec",
-        dealOrder: "tableThenPlayers",
-      },
-    );
-    expect(result.table.length).toBe(4);
-    for (const p of result.players) {
-      expect(p.hand.length).toBe(3);
-    }
-  });
-  it("should deal only players", () => {
-    const state = chooseDealer(stateWithDealer);
-    const result = dealRound(state, {
-      isDealerFirstDeal: false,
-    });
-    expect(result.table.length).toBe(0);
-    for (const p of result.players) {
-      expect(p.hand.length).toBe(3);
-    }
-  });
   it("should apply table bonus", () => {
     const deck = createDeck();
     const state = applyTablePatternBonus(
